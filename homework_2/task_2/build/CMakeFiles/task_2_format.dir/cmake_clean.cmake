18cc03d85cf0aa33cc22f9aa9e7c59aab6064f62
dheerajb07 <bogishettydheeraj007@gmail.com>
2025-01-06 16:29:24 -0500

    delete build files

--- homework_2/task_2/build/CMakeFiles/task_2_format.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/task_2_format"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/task_2_format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
