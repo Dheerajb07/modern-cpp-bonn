1d171b30b561223398d13aa9f66b1edf364c8b85
dheerajb07 <bogishettydheeraj007@gmail.com>
2025-03-09 17:53:39 -0400

    hw6 part1 complete

--- homework_6/include/image.hpp
+++ homework_6/include/image.hpp
@@ -8,23 +8,29 @@
 namespace igg {
 class Image {
 private:
-  int rows_;
-  int cols_;
+  int rows_ = 0;
+  int cols_ = 0;
   uint8_t max_val_ = 255;
   std::vector<uint8_t> data_{};
 
 public:
   Image(){};
-  Image(int rows, int cols) : rows_(rows), cols_(cols) {};
+  Image(int rows, int cols) : rows_(rows), cols_(cols) {
+    data_.resize(rows * cols, 0);
+  };
 
   int rows() const { return rows_; };
   int cols() const { return cols_; };
-  uint8_t max_value() const {return max_val_;};
-  const std::vector<uint8_t>& data() const {return data_;};
+  uint8_t max_value() const { return max_val_; };
+  const std::vector<uint8_t> &data() const { return data_; };
 
+  // retrieve pixel value at index (row,col)
+  uint8_t at(int row, int col) const { return data_[row * cols_ + col]; };
+
+  // fill pixel value at index (row,col)
   uint8_t &at(int row, int col) { return data_[row * cols_ + col]; };
 
-  bool FillFromPgm(const std::string &file_name); 
+  bool FillFromPgm(const std::string &file_name);
 
   void WriteToPgm(const std::string &file_name);
 };
