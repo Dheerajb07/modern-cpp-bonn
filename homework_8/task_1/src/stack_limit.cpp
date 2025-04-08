0321e7988c762fe7bb7b8a2b1080eb5f3919783a
dheerajb07 <bogishettydheeraj007@gmail.com>
2025-04-08 14:18:31 -0400

    hw_8 task_1

--- homework_8/task_1/src/stack_limit.cpp
+++ homework_8/task_1/src/stack_limit.cpp
@@ -1,9 +1,16 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
 
-int main(){
-    int size = 100;
-    while (true){
-        double* arr_ptr = new double[size];
-    }
-    return 0;
+void allocate_memory_on_stack(int i) {
+  std::array<double, (100 * 1024 / sizeof(double))> arr;
+  std::fill(arr.begin(), arr.end(), 3.14);
+  i += 100;
+  std::cerr << i << "[KiB] Allocated in the stack" << std::endl;
+  allocate_memory_on_stack(i);
+}
+
+int main() {
+  allocate_memory_on_stack(0);
+  return 0;
 }
